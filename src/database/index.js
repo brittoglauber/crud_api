5a39e0714ff8739dfd9c9d0a78a2cdc3787880e8
brittoglauber <brittoglauber@gmail.com>
2022-03-08 13:54:35 -0300

    firt commit

---
+++ src/database/index.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+import config from '../config/database'
+
+class Database {
+  constructor() {
+    this.connection = mongoose.connect(config.url, {
+      useUnifiedTopology: true
+    })
+  }
+}
+
+export default new Database()
